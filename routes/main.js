ad18bcecbb02c31f547f81cc345b61601903dcc1
hyowon3434 <hyowon3434@naver.com>
2024-08-05 16:25:19 +0900

    게시물 조회

--- routes/main.js
+++ routes/main.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const mainLayout = '../views/layouts/main.ejs'
+const Post = require('../models/Post')
+const asyncHandler = require('express-async-handler')
 
-router.get(['/','/home'], (req, res) => {
+router.get(['/','/home'], asyncHandler(async (req, res) => {
     const locals = {
         title: 'Home'
     }
-    res.render('index', {locals, layout: mainLayout})
-})
+
+    const data = await Post.find()
+    res.render('index', {locals, data, layout: mainLayout})
+}))
 
 router.get('/about', (req, res) => {
     const locals = {
@@ -16,4 +20,38 @@ router.get('/about', (req, res) => {
     res.render('about', {locals, layout: mainLayout})
 })
 
+// 게시물 상세 보기
+// GET /post/:id
+
+router.get('/post/:id', asyncHandler( async (req, res) => {
+    const data = await Post.findOne({_id:req.params.id})
+    res.render('post', {data, layout: mainLayout})
+}))
+
 module.exports = router
+
+
+
+
+// Post.insertMany([
+//     {
+//         title: '제목 1',
+//         body: '내용 1'
+//     },
+//     {
+//         title: '제목 2',
+//         body: '내용 2'
+//     },
+//     {
+//         title: '제목 3',
+//         body: '내용 3'
+//     },
+//     {
+//         title: '제목 4',
+//         body: '내용 4'
+//     },
+//     {
+//         title: '제목 5',
+//         body: '내용 5'
+//     },
+// ])
