649520813cc904ff7de2282036a20f5a249ae48f
hyowon3434 <hyowon3434@naver.com>
2024-08-07 11:55:19 +0900

    admin post 기능구현

--- routes/admin.js
+++ routes/admin.js
@@ -10,6 +10,22 @@ const jwt = require('jsonwebtoken')
 const jwtSecret = process.env.JWT_SECRET;
 
 
+// Check Login
+const checkLogin = (req, res, next) => { 
+    const token = req.cookies.token
+    if (!token) {
+        res.redirect('/admin')
+    } else{
+        try {
+            const decoded = jwt.verify(token, jwtSecret)
+            req.userId = decoded.userId
+            next()
+        } catch (error) {
+            res.redirect('/admin')
+        }
+    }
+}
+
 // Admin Page
 // GET /admin
 
@@ -64,7 +80,7 @@ router.post('/register', asyncHandler(async (req, res) => {
 // Get all Posts
 // GET /allPosts
 
-router.get('/allPosts', asyncHandler(async (req, res) => {
+router.get('/allPosts', checkLogin, asyncHandler(async (req, res) => {
     const locals = {
         title: 'Posts'
     }
@@ -79,5 +95,30 @@ router.get('/logout', (req, res) => {
     res.clearCookie('token')
     res.redirect('/')
 })
+
+// Admin - Add Post
+// GET /add
+
+router.get('/add', checkLogin, asyncHandler(async (req, res) => {
+    const locals = {
+        title: '게시물 작성'
+    }
+    res.render('admin/add', {locals, layout: adminLayout})
+}))
+
+// Admin - Add Post
+// POST /add
+
+router.post('/add', checkLogin, asyncHandler(async (req, res) => {
+    const {title, body} = req.body
+    const newPost = new Post({
+        title: title,
+        body: body,
+    })
+
+    await Post.create(newPost)
+    res.redirect('/allPosts')
+}))
+
 
 module.exports = router 
